64eea56d3270157712b62d8d4e929e8fdec2cc1e
Josh Glover <jmglov@gmail.com>
2016-05-30 16:30:08 +0200

    Get locations

--- src/klarna_demo/core.clj
+++ src/klarna_demo/core.clj
@@ -1,13 +1,37 @@
 (ns klarna-demo.core
   (:require [cheshire.core :as json]
-            [clj-http.client :as http])
+            [clj-http.client :as http]
+            [clojure.string :as string])
   (:gen-class))
 
+(def ^:private base-uri "http://www.ncdc.noaa.gov/cdo-web/api/v2")
+(def ^:private token "tvFfqDXOZiffOpXZMzuXSJufcBBaDbxL")
+
+(def ^:private location-types
+  {::city "CITY"
+   ::country "CNTRY"})
+
+(defn- make-query [params]
+  (some->> params
+           (map (fn [[k v]] (str (name k) "=" v)))
+           (string/join "&")
+           (str "?")))
+
+(defn- get-data
+  ([uri] (get-data uri nil))
+  ([uri params]
+   (-> (str base-uri uri (make-query params))
+       (http/get {:headers {:token token}})
+       :body
+       (json/parse-string true))))
+
 (defn location-categories []
-  (-> (http/get "http://www.ncdc.noaa.gov/cdo-web/api/v2/locationcategories"
-                {:headers {:token "tvFfqDXOZiffOpXZMzuXSJufcBBaDbxL"}})
-      :body
-      (json/parse-string true)))
+  (get-data "/locationcategories"))
+
+(defn locations [type]
+  (get-data "/locations"
+            {:locationcategoryid (location-types type)
+             :limit 5}))
 
 (defn -main
   "I don't do a whole lot ... yet."
